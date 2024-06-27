0cc3b6d73abd8caabd0fff4875e603a29e1128d1
godwinsallah16 <godwinsallah16@gmail.com>
2024-06-27 20:55:21 +0000

    done with pascal triangle

---
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -0,0 +1,23 @@
+def pascal_triangle(n):
+    """
+    Generate Pascal's Triangle up to n rows.
+    Args:
+        n (int): The number of rows of the triangle.
+    Returns:
+        list: A list of lists representing Pascal's Triangle.
+    """
+    try:
+        if not isinstance(n, int) or n <= 0:
+            raise ValueError("The number of rows must be a positive integer. Please retry with a positive number.")
+
+        triangle = [[1]]
+        for i in range(1, n):
+            row = [1]
+            for j in range(1, i):
+                row.append(triangle[i-1][j-1] + triangle[i-1][j])
+            row.append(1)
+            triangle.append(row)
+        return triangle
+    except ValueError as e:
+        print(e)
+        return []
---
+++ 0x00-pascal_triangle/README.md
@@ -0,0 +1,5 @@
+A python based projects that prints a traigle made of arrays
+Each row of array indicates the coefficien of polynomial functions
+of the form (x+a)^n with increasing power n
+
+
